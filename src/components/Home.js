7f38e42e1de566cd57728de76fd8a3615b0b827f
VARUNvk1729 <varunkakathiya1220@gmail.com>
2023-01-28 16:27:08 +0530

    Second commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components';
+import LeftSide from './LeftSide';
+import RightSide from './RightSide';
+import Main  from './Main';
 const Home = () => {
   return (
     <Container>
@@ -7,6 +10,12 @@ const Home = () => {
         <h5> <a>Hiring in a hurry? -</a></h5>
         <p>Find talented pros in record time with Upwork  and keep business moving</p>
       </Section>
+
+      <Layout>
+        <LeftSide/>
+        <Main/>
+        <RightSide/>
+      </Layout>
     </Container>
   )
 }
@@ -37,8 +46,32 @@ h5{
 }
 p{
   font-size: 14px;
+  color:#434649;
+  font-weight: 600;
 }
 
+@media (max-width : 768px)
+{
+  flex-direction:column;
+  padding:0 5px;
+
+ 
+}
 `;
 
+const Layout=styled.div`
+   display : grid;
+   grid-template-areas:"leftside main rightside";
+   grid-template-columns: minmax(0,5fr) minmax(0,12fr) minmax(300px,7fr);
+   column-gap : 25px;
+   row-gap: 25px;
+   grid-template-rows : auto;
+   margin: 25px;
+   @media (max-width : 768px)
+   {
+     display: flex;
+     flex-direction:column;
+     padding:0 5px;
+   }
+`;
 export default Home
---
+++ src/components/LeftSide.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+const LeftSide = (props) => {
+    return <Container>LeftSide</Container>
+};
+
+const Container=styled.div`
+   grid-area:leftside;
+`;
+
+export default LeftSide;
